0b81c718fb7251c44eca0f56c2cd302ae4035363
Amol Arora <amolarora99@yahoo.com>
2022-05-27 18:53:52 +1200

    fixed details form banner

--- src/components/DetailsBanner.js
+++ src/components/DetailsBanner.js
@@ -9,13 +9,13 @@ function DetailsBanner() {
                     <h1 id = "yourDetails">1. Your Details</h1>
                 </div>
                 <div className = "dash">
-                    <img src={rectangle} width="150" alt=""></img>
+                    <img src={rectangle} width="100" alt=""></img>
                 </div>
                 <div className = "detailsBannerCard">
                     <h2>2. Choose your policy</h2>
                 </div>
                 <div className = "dash">
-                    <img src={rectangle} width="150" alt=""></img>
+                    <img src={rectangle} width="100" alt=""></img>
                 </div>
                 <div className = "detailsBannerCard">
                     <h2>3. Review</h2>
